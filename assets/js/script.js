e3560e0e125e41fec130ed8c6bbb5b101cc2a2ce
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-04-19 21:30:08 -0400

    Centered buttons and adjusted font size

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,63 +9,61 @@ $(document).ready(function() {
 
 
 /* Click the button to display the joke */
-document.getElementById("jokeButton").addEventListener("click",function(event) {
+document.getElementById("jokeButton").addEventListener("click", function(event) {
     event.preventDefault();
-    var requestUrl="https://v2.jokeapi.dev/joke/"
+    var requestUrl = "https://v2.jokeapi.dev/joke/"
 
-        /*This will detect and filter out jokes that are blacklisted */
-    var ourFilter = $("#filters").val(); 
+    /*This will detect and filter out jokes that are blacklisted */
+    var ourFilter = $("#filters").val();
     var category = $("#categories").val();
     console.log(ourFilter)
     console.log(category)
     if (category.length > 0) {
-        requestUrl = requestUrl+category;
+        requestUrl = requestUrl + category;
         console.log(category)
         console.log(requestUrl)
     } else {
-        requestUrl = requestUrl+"Any";
+        requestUrl = requestUrl + "Any";
     }
     if (ourFilter.length > 0) {
-        requestUrl = requestUrl+"?blacklistFlags="+ourFilter;
+        requestUrl = requestUrl + "?blacklistFlags=" + ourFilter;
         console.log(ourFilter)
         console.log(requestUrl)
     }
     console.log(requestUrl)
-/* Var request joke api with blacklist */
-   // var requestUrl="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+        /* Var request joke api with blacklist */
+        // var requestUrl="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
 
 
 
-    
+
     function getRandomJoke() {
         fetch(requestUrl).then(function(response) {
-            if(!response.ok) {
-                document.getElementById("joke1").innerText="There was an error retriving the joke."
-                document.getElementById("joke2").innerHTML=""
-            } 
+            if (!response.ok) {
+                document.getElementById("joke1").innerText = "There was an error retriving the joke."
+                document.getElementById("joke2").innerHTML = ""
+            }
             return response.json();
         }).then(function(data) {
             console.log(data);
             /* This part displays the joke */
-            if(data.type=="twopart") {
-                var jokeSetup=data.setup
-                var jokeDelivery=data.delivery
-                document.getElementById("joke1").innerText=jokeSetup
-                document.getElementById("joke2").innerText=jokeDelivery
+            if (data.type == "twopart") {
+                var jokeSetup = data.setup
+                var jokeDelivery = data.delivery
+                document.getElementById("joke1").innerText = jokeSetup
+                document.getElementById("joke2").innerText = jokeDelivery
             } else {
-                var oneLineJoke=data.joke
-                document.getElementById("joke1").innerText=oneLineJoke
-                document.getElementById("joke2").innerHTML=""
+                var oneLineJoke = data.joke
+                document.getElementById("joke1").innerText = oneLineJoke
+                document.getElementById("joke2").innerHTML = ""
             }
             share();
             return;
         });
     }
     getRandomJoke()
-    
 
-
-    })
+})
 
 
 /* test */
@@ -91,7 +89,7 @@ function share() {
     var jokeOutput2 = document.getElementById("joke2").textContent;
     //this grabs the URL of the current webpage you're on 
     let postURL = encodeURI(document.location.href);
-    let postTitle = encodeURI(jokeOutput+jokeOutput2);
+    let postTitle = encodeURI(jokeOutput + jokeOutput2);
 
     //console.log(postTitle); 
 
